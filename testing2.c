0be747fb215781ed58cd75f8ed304994f5a6b472
Corentin-LG <corentin.legoff@epfedu.fr>
2023-10-08 17:36:24 +0200

    t2 dont work

--- testing2.c
+++ testing2.c
@@ -16,10 +16,10 @@ struct ConversionTable
 struct ConversionTable tableX[] = {
     {L"Tu", 1}, {L"Tr", 2}, {L"Td", 3}, {L"Tl", 4}, {L"D", 5}, {L"b", 6}, {L"w", 7}, {L"Bs", 8}, {L"Bm", 9}, {L"B", 10}, {L"Au", 11}, {L"Ar", 12}, {L"Ad", 13}, {L"Al", 14}, {L"Mur", 15}, {L"Mdr", 16}, {L"Mdl", 17}, {L"Mlu", 18}, {L"Wu", 19}, {L"Wr", 20}, {L"Wd", 21}, {L"Wl", 22}, {L"C", 23}, {L"Rur", 24}, {L"Rrd", 25}, {L"Rdl", 26}, {L"Rlu", 27}, {L"I", 28}, {L"i", 29}, {L"Tr", 30}, {L"Tg", 31}, {L"Tb", 32}, {L"Tc", 33}, {L"Ty", 34}, {L"Tp", 35}, {L"Tw", 36}, {L"Td", 37}};
 
-
 int main()
 {
     srand(time(NULL));
+    //const char *filename = "Beginner-I.lt4";
     const char *filename = "testing.lt4";
     int numRows = 0;
     int numColumns = 0;
@@ -35,7 +35,7 @@ int main()
     }
 
     // Recherche du nombre de lignes
-    wchar_t header[256];
+    wchar_t header[100000];
     fgetws(header, sizeof(header) / sizeof(header[0]), file);
     if (swscanf(header, L"Rows: %d", &numRows) != 1)
     {
@@ -54,10 +54,10 @@ int main()
     }
 
     // Créez un tableau 2D pour stocker les valeurs converties
-    int **tableau = (int **)malloc(numRows * sizeof(int *));
+    int **tableau = (int **)malloc((numRows + 1) * sizeof(int *));
     for (int i = 0; i < numRows; i++)
     {
-        tableau[i] = (int *)malloc(numColumns * sizeof(int));
+        tableau[i] = (int *)malloc((numColumns + 1) * sizeof(int));
     }
 
     for (int i = 0; i < 5; i++)
@@ -66,25 +66,31 @@ int main()
     }
 
     wchar_t *token;
+    wchar_t *splitBuffer;
 
     // Lire les données du fichier et les stocker dans le tableau
     for (int i = 0; i < numRows; i++)
     {
         fgetws(header, sizeof(header) / sizeof(header[0]), file);
 
+        wprintf(L"En-tête complet : %ls\n", header);
         for (int j = 0; j < numColumns; j++)
         {
-            wprintf(L"En-tête complet : %ls\n", header);
             token = wcstok(header, L" ");
-
+            wprintf(L"Token complet : %ls\n", token);
             // Recherche de la correspondance dans la table de conversion
-            for (int k = 0; k < sizeof(tableX) / sizeof(tableX[0]); k++)
+            while (token != NULL)
             {
-                if (wcscmp(token, tableX[k].lettre) == 0)
+                for (int k = 0; k < sizeof(tableX) / sizeof(tableX[0]); k++)
                 {
-                    tableau[i][j] = tableX[k].valeur;
-                    break;
+                    if (wcscmp(token, tableX[k].lettre) == 0)
+                    {
+                        tableau[i][j] = tableX[k].valeur;
+                        break;
+                    }
                 }
+                //wprintf(L"%ls\n", token);
+                token = wcstok(NULL, L" ");
             }
         }
     }
